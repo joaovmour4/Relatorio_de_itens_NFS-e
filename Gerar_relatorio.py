d67e6a0f72f4e18d999d4f289db7a78a70ed31ba
joaovmour4 <joaov.mour@unifesspa.edu.br>
2023-02-23 17:31:30 -0300

    Primeiro commit

---
+++ Gerar_relatorio.py
@@ -0,0 +1,108 @@
+from tkinter import *
+from tkinter.filedialog import *
+import xml.etree.ElementTree as Et
+import openpyxl
+from openpyxl.styles.borders import Border, Side
+
+
+def collect_arquivo(entry):
+    entry.insert(0, askopenfilename())
+
+
+def save_arquivo(entry):
+    entry.insert(0, askdirectory())
+
+
+def executar(arquivo, directory):
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+
+    xml = Et.parse(f'{arquivo}')
+
+    root = xml.getroot()
+    i = 1
+    blueFill = openpyxl.styles.PatternFill(start_color='99CCFF',
+                                           end_color='99CCFF',
+                                           fill_type='solid')
+
+    blue1Fill = openpyxl.styles.PatternFill(start_color='CCECFF',
+                                            end_color='CCECFF',
+                                            fill_type='solid')
+
+    thin_border = Border(left=Side(style='thin'),
+                         right=Side(style='thin'),
+                         top=Side(style='thin'),
+                         bottom=Side(style='thin'))
+
+    sheet.column_dimensions['B'].width = 16
+    sheet.column_dimensions['C'].width = 16
+    sheet.column_dimensions['D'].width = 30
+    sheet.column_dimensions['E'].width = 16
+    sheet.column_dimensions['F'].width = 25
+
+    for nota in root:
+        cll_nota = sheet.cell(row=i, column=2)
+        sheet.merge_cells(f'B{i}:F{i}')
+        cll_nota.alignment = openpyxl.styles.Alignment(horizontal='center')
+        cll_nota.value = f'nota {nota.find("tcInfNFE")[0].text}'
+        cll_nota.fill = blueFill
+        i += 1
+        for elemento in nota:
+            if elemento.tag == 'tcInfNFE':
+                cll_elemento = sheet.cell(row=i, column=2)
+                for campo in elemento:
+                    if campo.tag == 'tcInfItens':
+                        cll_campo = sheet.cell(row=i, column=3)
+                        cll_campo.alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+                        for item in campo:
+                            j = 2
+                            cll_item = sheet.cell(row=i, column=j)
+                            cll_item.fill = blue1Fill
+                            sheet.merge_cells(f'B{i}:F{i}')
+                            cll_item.alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+                            cll_item.value = f'Informações do Item'
+                            i += 1
+                            for detalhe in item:
+                                cll_detalhe_info = sheet.cell(row=i, column=j)
+                                cll_detalhe = sheet.cell(row=i + 1, column=j)
+                                cll_detalhe.alignment = openpyxl.styles.Alignment(horizontal='center',
+                                                                                  vertical='center', wrapText=True)
+                                cll_detalhe_info.alignment = openpyxl.styles.Alignment(horizontal='center',
+                                                                                       vertical='center')
+                                j += 1
+                                match str(detalhe.tag):
+                                    case 'tsSeqItem':
+                                        cll_detalhe_info.value = f'ID item'
+                                    case 'tsQtdItem':
+                                        cll_detalhe_info.value = f'Quantidade'
+                                    case 'tsDesItem':
+                                        cll_detalhe_info.value = f'Descrição'
+                                    case 'tsVlrItem':
+                                        cll_detalhe_info.value = f'Valor do item'
+                                    case 'tsTotItem':
+                                        cll_detalhe_info.value = f'Valor total dos itens'
+                                cll_detalhe.value = f'{detalhe.text}'
+                            i += 3
+                        i += 2
+        i += 2
+
+    wb.save(f'{directory}/Relatorio.xlsx')
+
+
+janela = Tk()
+janela.geometry('400x200')
+janela.title('Gerar relatório de NFS-e')
+
+entry_arquivo = Entry(janela)
+entry_arquivo.grid(column=0, row=0, padx=10)
+
+entry_save = Entry(janela)
+entry_save.grid(column=0, row=1, padx=10)
+
+button_arquivo = Button(janela, text='Selecionar arquivo xml', command=lambda:collect_arquivo(entry_arquivo)).grid(column=1, row=0)
+button_save = Button(janela, text='Selecionar pasta de salvamento', command=lambda:save_arquivo(entry_save)).grid(column=1, row=1)
+button_exec = Button(janela, text='Executar', command=lambda:executar(entry_arquivo.get(), entry_save.get())).grid(column=2, row=3)
+
+
+
+janela.mainloop()
